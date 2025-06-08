c05b41e9a266f808fdd5e75edd4fb7718f54cba2
n0tx <rcandra91@msn.com>
2025-06-09 03:15:15 +0700

    menambahkan angular directive dengan angular cli ng g directive directives/highlight-completed-todo membuat directive untuk style pada list item yang di centang dan tidak di centang menggunakan directive HighlightCompletedTodo pada komponen list todo ts dan html
    HighlightCompletedTodo, class, buat file ts list todo nya
    highlightCompletedTodo, selector, buat  list todo htmlnya

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,9 +1,10 @@
 import { Component, input } from "@angular/core";
 import { Todo } from "../../model/todo.type";
+import { HighlightCompletedTodo } from "../../directives/highlight-completed-todo";
 
 @Component({
     selector: "app-todo-item",
-    imports: [],
+    imports: [HighlightCompletedTodo],
     templateUrl: "./todo-item.html",
     styleUrl: "./todo-item.css"
 })
