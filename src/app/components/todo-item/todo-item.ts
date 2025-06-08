6ce4477d19a3b4749c679c8cab2710dfcc3903b4
n0tx <rcandra91@msn.com>
2025-06-08 23:51:35 +0700

    memindahkan list item todo dari todo.html ke todo-item.html berikut dengan file ts dan css nya

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from "@angular/core";
+import { Todo } from "../../model/todo.type";
 
 @Component({
-  selector: 'app-todo-item',
-  imports: [],
-  templateUrl: './todo-item.html',
-  styleUrl: './todo-item.css'
+    selector: "app-todo-item",
+    imports: [],
+    templateUrl: "./todo-item.html",
+    styleUrl: "./todo-item.css"
 })
 export class TodoItem {
-
+    todoItem = input.required<Todo>();
 }
