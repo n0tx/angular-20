fd95aa92a4bb1d8083e198af6bb1d8526bc5aec1
n0tx <rcandra91@msn.com>
2025-06-09 18:55:51 +0700

    membuat pipe sendiri generate pipe dengan angular cli ng g pipe pipes/filter-todos membuat searchTerm signal, untuk text pencariannya yang dikaitkan dengan ngModel membuat form bawaan angular FormsModule agar diproses oleh angular pipe filter.ts, mencari yang mengandung teks pencarian, kembalikan sebagai todo array menempelkan pipe pada todo komponen

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit, signal } from "@angular/core";
 import { TodoItem } from "../components/todo-item/todo-item";
 import { TodosService } from "../services/todos.service";
 import { Todo } from "../model/todo.type";
+import { FormsModule } from "@angular/forms";
+import { FilterTodosPipe } from "../pipes/filter-todos-pipe";
 
 @Component({
     selector: "app-todos",
-    imports: [TodoItem],
+    imports: [TodoItem, FormsModule, FilterTodosPipe],
     templateUrl: "./todos.html",
     styleUrl: "./todos.css"
 })
@@ -13,6 +15,7 @@ export class Todos {
     todosService = inject(TodosService);
     todoItems = signal<Array<Todo>>([]);
     todoItemsFromApi = signal<Array<Todo>>([]);
+    searchTerm = signal("");
     ngOnInit(): void {
         console.log("todo hardcoded: ", this.todosService.todoItems);
 
@@ -27,16 +30,16 @@ export class Todos {
     }
 
     updateTodoItem(todoItem: Todo) {
-      this.todoItemsFromApi.update((todos) => {
-        return todos.map((todo) => {
-          if (todo.id === todoItem.id) {
-            return {
-              ...todo,
-              completed: !todo.completed,
-            };
-          }
-          return todo;
+        this.todoItemsFromApi.update(todos => {
+            return todos.map(todo => {
+                if (todo.id === todoItem.id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    };
+                }
+                return todo;
+            });
         });
-      });
     }
 }
